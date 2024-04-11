12e17cf96a0fcec777288f416b7f41c30a99a181
Gustavo Ferreira <gustavoaferreira@outlook.com.br>
2024-04-11 10:18:12 -0300

    contador

--- CursoCucumber/src/test/java/steps/AprenderCucumber.java
+++ CursoCucumber/src/test/java/steps/AprenderCucumber.java
@@ -3,24 +3,39 @@ package steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import junit.framework.Assert;
 
 public class AprenderCucumber {
 
 	@Given("que criei o arquivo corretamente")
 	public void que_criei_o_arquivo_corretamente() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+
 	}
 
 	@When("executa-lo")
 	public void executa_lo() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+
 	}
 
 	@Then("a especificacao deve finalizar com sucesso")
 	public void a_especificacao_deve_finalizar_com_sucesso() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+
+	}
+	
+	private int contador = 0;
+	
+	@Given("que o valor do contador e (\\d+)$")
+	public void que_o_valor_do_contador(Integer int1) {
+	    contador = int1;
+	}
+
+	@When("eu incrementar em (\\d+)$")
+	public void eu_incrementar_em(Integer int1) {
+	    contador = contador + int1;
+	}
+
+	@Then("o valor do contador sera (\\d+)$")
+	public void o_valor_do_contador_ser(Integer int1) {
+	    Assert.assertEquals(int1.doubleValue(), Double.valueOf(contador));
 	}
 }
--- CursoCucumber/src/test/resources/features/aprender_cucumber.feature
+++ CursoCucumber/src/test/resources/features/aprender_cucumber.feature
@@ -7,3 +7,8 @@ Scenario: Deve executar especificação
 	Given que criei o arquivo corretamente
 	When executa-lo
 	Then a especificacao deve finalizar com sucesso
+	
+Scenario: Deve incrementar contador
+	Given que o valor do contador e 15
+	When eu incrementar em 3
+	Then o valor do contador sera 18
