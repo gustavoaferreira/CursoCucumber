a507f3fd313872e753db694bb5ad8cdf2f119cbd
Gustavo Ferreira <gustavoaferreira@outlook.com.br>
2024-04-11 10:43:37 -0300

    Reaproveitando cenarios

--- CursoCucumber/src/test/java/steps/AprenderCucumber.java
+++ CursoCucumber/src/test/java/steps/AprenderCucumber.java
@@ -1,5 +1,10 @@
 package steps;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -37,5 +42,36 @@ public class AprenderCucumber {
 	@Then("o valor do contador sera (\\d+)$")
 	public void o_valor_do_contador_ser(Integer int1) {
 	    Assert.assertEquals(int1.doubleValue(), Double.valueOf(contador));
+	}
+	
+	Date entrega = new Date();
+	
+	@Given("que a entrega e dia (\\d+)/(\\d+)/(\\d+)$")
+	public void que_a_entrega_e_dia(Integer dia, Integer mes, Integer ano) {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, dia);
+		cal.set(Calendar.MONTH, mes - 1);
+		cal.set(Calendar.YEAR, ano);
+		entrega = cal.getTime();
+	}
+
+	@When("a entrega atrasa em (\\d+) (dia|dias|mes|meses)$")
+	public void a_entrega_atrasa_em_dias(Integer int1, String tempo) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(entrega);
+		if(tempo.equals("dias")) {
+			cal.add(Calendar.DAY_OF_MONTH, int1);
+		}
+		if(tempo.equals("meses")) {
+			cal.add(Calendar.MONTH, int1);
+		}
+		entrega = cal.getTime();
+	}
+
+	@Then("a entrega sera efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+	public void a_entrega_sera_efetuada_em(String data) {
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String dataFormatada = format.format(entrega);
+		Assert.assertEquals(data, dataFormatada);
 	}
 }
--- CursoCucumber/src/test/resources/features/aprender_cucumber.feature
+++ CursoCucumber/src/test/resources/features/aprender_cucumber.feature
@@ -3,7 +3,7 @@ Feature: Aprender Cucumber
 	Eu quero aprender a utilizar Cucumber
 	Para que eu possa automatizar critérios de aceitação
 	
-Scenario: Deve executar especificação
+Scenario: Deve executar especificacao
 	Given que criei o arquivo corretamente
 	When executa-lo
 	Then a especificacao deve finalizar com sucesso
@@ -12,3 +12,13 @@ Scenario: Deve incrementar contador
 	Given que o valor do contador e 15
 	When eu incrementar em 3
 	Then o valor do contador sera 18
+	
+Scenario: Deve incrementar contador
+	Given que o valor do contador e 123
+	When eu incrementar em 35
+	Then o valor do contador sera 158
+	
+Scenario: Deve calcular atraso na entrega
+	Given que a entrega e dia 05/04/2018
+	When a entrega atrasa em 2 dias
+	Then a entrega sera efetuada em 07/04/2018
