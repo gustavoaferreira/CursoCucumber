b1243fc054cf34210a702c2bdf96f9e0dbfb88c2
Gustavo Ferreira <gustavoaferreira@outlook.com.br>
2024-04-11 10:00:31 -0300

    first commit

---
+++ CursoCucumber/src/test/java/steps/AprenderCucumber.java
@@ -0,0 +1,26 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AprenderCucumber {
+
+	@Given("que criei o arquivo corretamente")
+	public void que_criei_o_arquivo_corretamente() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("executa-lo")
+	public void executa_lo() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("a especificacao deve finalizar com sucesso")
+	public void a_especificacao_deve_finalizar_com_sucesso() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
